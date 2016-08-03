993fcad0e9b9bffaba07ccde156cc2d041534673
Fleischers <Fleischer458@gmail.com>
2016-08-03 22:24:44 +0300

    Init Angular2 Webpack with help of @AngularClass

---
+++ src/app/app.component.ts
@@ -0,0 +1,75 @@
+/*
+ * Angular 2 decorators and services
+ */
+import { Component, ViewEncapsulation } from '@angular/core';
+
+import { AppState } from './app.service';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.style.css'
+  ],
+  template: `
+    <nav>
+      <span>
+        <a [routerLink]=" ['./'] ">
+          Index
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./home'] ">
+          Home
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./detail'] ">
+          Detail
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./about'] ">
+          About
+        </a>
+      </span>
+    </nav>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+
+    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+
+    <footer>
+      <span>Seven Wonders Score Started by <a [href]="url">@GorlifSense</a></span>
+      <div>
+        <a [href]="url">
+          <img [src]="angularclassLogo" width="25%">
+        </a>
+      </div>
+    </footer>
+  `
+})
+export class App {
+  angularclassLogo = 'assets/img/angularclass-avatar.png';
+  name = 'Seven Wonders Score';
+  url = 'https://twitter.com/GorlifSense';
+
+  constructor(
+    public appState: AppState) {
+
+  }
+
+  ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
