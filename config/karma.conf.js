4c671a454c59f5d14e1d88a6a371295012ac24cc
Fleischers <Fleischer458@gmail.com>
2016-08-08 17:08:19 +0300

    Test of Firefox for Travis CI

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-      'Chrome'
+      'Firefox'
     ],
 
     /*
