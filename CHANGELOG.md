222c9de26491780fd3eb97a0e7e1bef549ce95c5
Fleischers <Fleischer458@gmail.com>
2017-01-29 23:20:40 +0200

    Angular CLI init
    Precommit priority

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Release notes for Seven Wonders Score
 
-## v0.0.1
+## v0.0.1-alpha.2
 
-* Init Angular2
+* Init Angular v2.3.1
+* Generate directory structure with Angular CLI
+
+## v0.0.1-alpha.1
+
+* Init Angular 2 RC4
 * Defined base directory structure
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,10 @@
-/*
- * Angular 2 decorators and services
- */
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { AppState } from './app.service';
-
-/*
- * App Component
- * Top Level Component
- */
 @Component({
-  selector: 'app',
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./app.style.css'],
-  templateUrl: 'app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-export class App {
-  angularclassLogo = 'assets/img/angularclass-avatar.png';
-  name = 'Seven Wonders Score';
-  url = 'https://twitter.com/GorlifSense';
-
-  constructor(
-    public appState: AppState) {
-
-  }
-
-  ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-  }
-
+export class AppComponent {
+  title = 'app works!';
 }
