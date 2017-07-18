cb562045edbdb364e3d9bb26080205e760068597
Fleischers <Fleischer458@gmail.com>
2017-07-18 13:58:57 +0300

    Navtigation component added

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class SevenWondersScorePage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('bgr-root h1')).getText();
   }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
-/* tslint:disable:no-unused-variable */
-
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavigationComponent
       ],
     });
     TestBed.compileComponents();
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Seven Wonders Score'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Seven Wonders Score');
