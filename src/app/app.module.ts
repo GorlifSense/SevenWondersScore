50c6dc28d106db7a99390df5deb1058936f318ab
Glib Ischenko <Fleischer458@gmail.com>
2017-08-08 17:33:20 +0300

    HttpClient makes GET /tables from BGR (#12)
    * HttpClient makes GET /tables from BGR
    * Fixed jasmine tests to see HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -20,7 +20,7 @@ import { TableItemComponent } from './tables/table-item/table-item.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
